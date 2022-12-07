59a10a25384ed54e9749aef1777cb40ee885a0a1
Niklas Sahlberg <niklas.sahlberg@yh.nackademin.se>
2022-12-07 23:22:17 +0100

    Modified variables in Item class matching frontend

--- src/main/java/teknikfordjupning/backend/model/Item.java
+++ src/main/java/teknikfordjupning/backend/model/Item.java
@@ -9,35 +9,61 @@ public class Item implements Serializable {
      public Item() {}
 
 
-    public Item(Long id, String brand, String type, String name, String itemCode) {
-        this.id = id;
-        this.brand = brand;
-        this.type = type;
-        this.name = name;
-        this.itemCode = itemCode;
+    public Double getPrice() {
+        return price;
     }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+    public Item(Long id, String name, String category, double price, String itemCode, String description, String image) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.itemCode = itemCode;
+        this.description = description;
+        this.image = image;
+    }
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable =false)
     private Long id;
-    private String brand;
-    private String type;
+
     private String name;
+
+
+
+    private String category;
+
+    private double price;
+
+
     private String itemCode;
+
 
     @Override
     public String toString() {
         return "Item{" +
                 "id=" + id +
-                ", brand='" + brand + '\'' +
-                ", type='" + type + '\'' +
                 ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
                 ", itemCode='" + itemCode + '\'' +
+                ", description='" + description + '\'' +
+                ", image='" + image + '\'' +
                 '}';
     }
 
-    public String getItemCode() {             
+    private String description;
+    private String image;
+
+
+    public String getItemCode() {
          return itemCode;
      }
 
@@ -49,24 +75,29 @@ public class Item implements Serializable {
         return id;
     }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getBrand() {
-        return brand;
+
+    public String getCategory() {
+        return category;
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
+    public void setCategory(String type) {
+        this.category = type;
     }
 
     public String getName() {
@@ -76,4 +107,7 @@ public class Item implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+    public String getImage() {return image;}
+
+    public void setImage(String image) {this.image = image;}
 }
